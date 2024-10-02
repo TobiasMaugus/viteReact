fcd649d754cca4d25545157a00412c15b94e8a40
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-10-02 08:43:03 -0300

    start header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -4,7 +4,11 @@ import "./header--styles";
 const Header: React.FC = ()=>{
     return(
         <div className="header--container">
-            
+            <nav>
+                <button>add</button>
+                <button>edit</button>
+                <button>remove</button>
+            </nav>
         </div>
     )
 }
