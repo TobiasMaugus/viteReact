ddec53b87d2f657947dfe7487689bbd4354e7c07
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-10-02 08:46:54 -0300

    start home

--- src/components/HomeBody/homebody.tsx
+++ src/components/HomeBody/homebody.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./homebody--styles";
+import Header from "../Header/header.tsx";
 
 const HomeBody: React.FC = ()=>{
     return(
         <div className="homeBody--container">
-            
+            <Header/>
         </div>
     )
 }
---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css"
+import HomeBody from "../../components/HomeBody/homebody";
+
+const Home: React.FC = () => {
+    return (
+        <div className="login--container">
+            <HomeBody/>
+        </div>
+        
+    )
+}
+
+export default Home
