5be357011c7ce27187b4f84393b21191fcda7467
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-10-03 10:04:45 -0300

    icons

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./header--styles.css";
+import addIcon from "../../assets/add.svg";
+import editIcon from "../../assets/edit.svg";
+import deleteIcon from "../../assets/delete.svg";
 
 const Header: React.FC = ()=>{
     return(
         <div className="header--container">
-                <button className="header--button">add</button>
-                <button className="header--button">edit</button>
-                <button className="header--button">remove</button>
+            <button className="header--button">
+                <img src={addIcon} alt="add-icon" className="button--icon"/>
+            </button>
+            <button className="header--button">
+                <img src={editIcon} alt="edit-icon" className="button--icon"/>
+            </button>
+            <button className="header--button">
+            <img src={deleteIcon} alt="delete-icon" className="button--icon"/>
+            </button>
         </div>
     )
 }
