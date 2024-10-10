977352c6719a991a3d5cfa06eb15b51aa16a2b03
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-10-10 09:31:55 -0300

    mudança de layout

--- src/components/HomeBody/homebody.tsx
+++ src/components/HomeBody/homebody.tsx
@@ -1,14 +1,40 @@
 import React from "react";
 import "./homebody--styles.css";
 import Header from "../Header/header.tsx";
-import Carousel from "../Carousel/Carousel.tsx";
+import HomeCard from "../HomeCard/HomeCard.tsx";
 
 const HomeBody: React.FC = ()=>{
     return(
         <div className="homeBody--container">
             <Header/>
-            <div className="carousel--container"> <Carousel /></div>
+            <div className="content--container">
+                <HomeCard title="Card 1" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 
+                ac erat et diam congue cursus. Nunc eget maximus lacus, a ultricies dolor.
+                Ut ut vehicula nibh. Maecenas tincidunt ligula sed luctus placerat."/>
+                <HomeCard title="Card 2" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 
+                ac erat et diam congue cursus. Nunc eget maximus lacus, a ultricies dolor.
+                Ut ut vehicula nibh. Maecenas tincidunt ligula sed luctus placerat."/>
+                <HomeCard title="Card 3" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 
+                ac erat et diam congue cursus. Nunc eget maximus lacus, a ultricies dolor.
+                Ut ut vehicula nibh. Maecenas tincidunt ligula sed luctus placerat."/>
+                <HomeCard title="Card 3" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 
+                ac erat et diam congue cursus. Nunc eget maximus lacus, a ultricies dolor.
+                Ut ut vehicula nibh. Maecenas tincidunt ligula sed luctus placerat."/>
+                <HomeCard title="Card 3" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 
+                ac erat et diam congue cursus. Nunc eget maximus lacus, a ultricies dolor.
+                Ut ut vehicula nibh. Maecenas tincidunt ligula sed luctus placerat."/>
+                <HomeCard title="Card 3" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 
+                ac erat et diam congue cursus. Nunc eget maximus lacus, a ultricies dolor.
+                Ut ut vehicula nibh. Maecenas tincidunt ligula sed luctus placerat."/>
+                 <HomeCard title="Card 3" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 
+                ac erat et diam congue cursus. Nunc eget maximus lacus, a ultricies dolor.
+                Ut ut vehicula nibh. Maecenas tincidunt ligula sed luctus placerat."/>
+                <HomeCard title="Card 3" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris 
+                ac erat et diam congue cursus. Nunc eget maximus lacus, a ultricies dolor.
+                Ut ut vehicula nibh. Maecenas tincidunt ligula sed luctus placerat."/>
+            </div>
         </div>
+
     )
 }
 export default HomeBody;
